ceb6ccf7de4c4386b459d07f56026acf3c509944
Nicolas Silva <nical.silva@gmail.com>
2011-04-04 09:27:37 +0200

    debut des structures

---
+++ SRC/structures/XmlElement.h
@@ -0,0 +1,18 @@
+
+#ifndef HEXALOUTRE_XMLELEMENT_H
+#define HEXALOUTRE_XMLELEMENT_H
+
+#include "structures/XmlContent.h"
+
+class XmlElement : public XmlContent
+{ 
+public:
+
+private:
+
+};
+
+
+
+
+#endif

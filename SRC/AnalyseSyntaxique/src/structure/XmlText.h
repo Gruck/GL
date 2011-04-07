ff347e19476b49171c6d2118f9427af6a3a34237
gael <gael@gael-1215P.(none)>
2011-04-07 14:52:19 +0200

    Gné?

--- SRC/AnalyseSyntaxique/src/structure/XmlText.h
+++ SRC/AnalyseSyntaxique/src/structure/XmlText.h
@@ -12,12 +12,15 @@ class XmlText : public XmlContent
 public:
   XmlText(const std::string& text)
   : XmlContent(0), _data(text) { }
+
+  ~XmlText(){}
   
   void toStream( std::ostream& stream, int indentation = 0 );
   
   std::string& text() { CALL_MACRO return _data; }
 
   bool acceptValidator( XmlValidatorVisitor* validator );
+
   
 private:
   std::string _data;

b600c86aadd0bf28ac7de7196491f718e8ca41bc
Gael MOTTE <gael.motte@gmail.com>
2011-04-07 14:57:37 +0200

    mais inline ca veut pas dire ca ;)

--- SRC/AnalyseSyntaxique/src/structure/XmlText.h
+++ SRC/AnalyseSyntaxique/src/structure/XmlText.h
@@ -13,12 +13,9 @@ public:
   XmlText(const std::string& text)
   : XmlContent(0), _data(text) { }
 
-<<<<<<< HEAD
+
   ~XmlText(){}
-=======
-  inline ~XmlText() {}
->>>>>>> 084300b81bcb9aaafc2e52ff24864586eba948d2
-  
+ 
   void toStream( std::ostream& stream, int indentation = 0 );
   
   std::string& text() { CALL_MACRO return _data; }

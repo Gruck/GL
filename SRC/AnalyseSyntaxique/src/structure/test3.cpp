fcfeec0a7e69a4e73c968b1c1b7594e3c1ac7f8e
Gael MOTTE <gael.motte@gmail.com>
2011-04-16 11:46:38 +0200

    oops, mauvais fichier ajouté

--- SRC/AnalyseSyntaxique/src/structure/test3.cpp
+++ SRC/AnalyseSyntaxique/src/structure/test3.cpp
@@ -12,24 +12,25 @@
 #include "XmlValidator.h"
 
 
-
-bool test2a();
-bool test2b();
+bool test3a();
+bool test3b();
 
 
 int main(){
-  assert( test2a() );
+  std::cout<<"Ce jeu de test a pour objectif de tester les fonctionalités liées à la validation des attributs\n";
+  assert( test3a() );
   std::cout << "\n\n\n";
-  assert( test2b() );
+  assert( test3b() );
   std::cout << "\n\n\n";
  
 }
 
-
-bool test2a(){
+bool test3a(){
+  std::cout << "======================================= Test Attrib a\n"; 
   std::cout << "--------------------------------------- create xml\n"; 
   XmlDoc* xml = new XmlDoc("html");
   xml->setRoot( new XmlElement("root") );
+  xml->root()->addAttribute("paramqeg","bouh");
   xml->root()->addChild( new XmlElement("foo") );
   xml->root()->childElement( "foo" )->addChild( new XmlText("du text") );
   xml->root()->childElement( "foo" )->addChild( new XmlElement("bar") );
@@ -37,9 +38,12 @@ bool test2a(){
   std::cout << "--------------------------------------- create dtd\n";
   DtdDoc* dtd = new DtdDoc("html");
   DtdPossibleContent* pc1 = new DtdPossibleContent(DtdPossibleContent::T_CHOICE);
+
   pc1->addChild(new DtdPossibleContent(DtdPossibleContent::T_ELEM,"foo"));
   pc1->addChild(new DtdPossibleContent(DtdPossibleContent::T_ELEM,"bar"));
-  dtd->AddElement( new DtdElement("root",pc1) );
+  DtdElement* root = new DtdElement("root",pc1);
+  dtd->AddElement( root );
+  root->addAttribute("param","#PCDATA");
   dtd->AddElement(new DtdElement("foo"
     , new DtdPossibleContent(DtdPossibleContent::T_ELEM,"#PCDATA") ) );
   dtd->AddElement(new DtdElement("bar"
@@ -57,25 +61,29 @@ bool test2a(){
   delete xml;
   std::cout << "--------------------------------------- delete dtd\n";
   delete dtd;
-
+  
   return status;
 }
 
-
-bool test2b(){
+bool test3a(){
+  std::cout << "======================================= Test Attrib b\n"; 
   std::cout << "--------------------------------------- create xml\n"; 
   XmlDoc* xml = new XmlDoc("html");
   xml->setRoot( new XmlElement("root") );
+  xml->root()->addAttribute("param","bouh");
   xml->root()->addChild( new XmlElement("foo") );
   xml->root()->childElement( "foo" )->addChild( new XmlText("du text") );
-  xml->root()->addChild( new XmlElement("bar") );
-  xml->root()->childElement( "bar" )->addChild( new XmlText("encore du text") );
+  xml->root()->childElement( "foo" )->addChild( new XmlElement("bar") );
+  xml->root()->childElement( "foo" )->childElement( "bar" )->addChild( new XmlText("encore du text") );
   std::cout << "--------------------------------------- create dtd\n";
   DtdDoc* dtd = new DtdDoc("html");
   DtdPossibleContent* pc1 = new DtdPossibleContent(DtdPossibleContent::T_CHOICE);
+
   pc1->addChild(new DtdPossibleContent(DtdPossibleContent::T_ELEM,"foo"));
   pc1->addChild(new DtdPossibleContent(DtdPossibleContent::T_ELEM,"bar"));
-  dtd->AddElement( new DtdElement("root",pc1) );
+  DtdElement* root = new DtdElement("root",pc1);
+  dtd->AddElement( root );
+  root->addAttribute("param","#PCDATA");
   dtd->AddElement(new DtdElement("foo"
     , new DtdPossibleContent(DtdPossibleContent::T_ELEM,"#PCDATA") ) );
   dtd->AddElement(new DtdElement("bar"
@@ -93,7 +101,6 @@ bool test2b(){
   delete xml;
   std::cout << "--------------------------------------- delete dtd\n";
   delete dtd;
-
+  
   return status;
 }
-

8ba4027ace2ec2ce7ea07fcdd02534339c70ee46
Gael MOTTE <gael.motte@gmail.com>
2011-04-17 17:27:16 +0200

    amélioration du ststeme de test

--- SRC/AnalyseSyntaxique/TEST_UNIT.sh
+++ SRC/AnalyseSyntaxique/TEST_UNIT.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
 
-./FullAnalyzer $1 && echo '    <test file="'$1'" status="success">' $2 '</test>' >> $3 || echo '    <test file="'$1'" status="failure">' $2 '</test>' >> $3
 
+if [ $3 = "reussite" ]
+then
+echo "Ceci est est cas qui devrait fonctioner \n"
+./FullAnalyzer $1 && echo '    <test file="'$1'" status="success">' $2 '</test>' >> $4 || echo '    <test file="'$1'" status="failure">' $2 '</test>' >> $4
+fi
+
+if [ $3 = "echec" ]
+then
+echo "Ceci est est cas qui devrait rater \n"
+./FullAnalyzer $1 && echo '    <test file="'$1'" status="success">' $2 '</test>' >> $4 || echo '    <test file="'$1'" status="failure">' $2 '</test>' >> $4
+fi
